a9ad3d40df4d8abac00763c53030c1f81f27444f
kphani <pkaliginadi@gmail.com>
2014-09-29 22:32:44 +0100

    resolve Driver type from Properties

--- src/main/java/edu/phani/selenium/help/DriverDiscovery.java
+++ src/main/java/edu/phani/selenium/help/DriverDiscovery.java
@@ -1,19 +1,55 @@
 package edu.phani.selenium.help;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Created by kphani on 27/09/2014.
  */
 public class DriverDiscovery {
-    public static WebDriver driver;
+    private static final String RUN_CONFIG_PROPERTIES = "/runConfig.properties";
+    private static RemoteWebDriver driver;
 
     static {
-        driver = new FirefoxDriver();
+        try {
+            getDriverTyp();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
     }
 
+    public static void getDriverTyp() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Properties props = new Properties();
+        InputStream is = DriverDiscovery.class.getResourceAsStream(RUN_CONFIG_PROPERTIES);
+        try {
+            props.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String driverType = props.getProperty("driverType");
+
+        Class<?> retrievedClass = Class.forName(driverType);
+        driver = (RemoteWebDriver) retrievedClass.newInstance();
+    }
 
 
+    public static RemoteWebDriver getDriver() {
+        return driver;
+    }
 }
+
+
+
+
+
+
+
+
--- src/test/java/edu/phani/selenium/tests/BaseTest.java
+++ src/test/java/edu/phani/selenium/tests/BaseTest.java
@@ -3,13 +3,13 @@ package edu.phani.selenium.tests;
 import edu.phani.selenium.help.DriverDiscovery;
 import edu.phani.selenium.pages.Navigation;
 import org.junit.AfterClass;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 /**
  * Created by kphani on 28/09/2014.
  */
 public class BaseTest {
-    final static WebDriver driver = DriverDiscovery.driver;
+    final static RemoteWebDriver driver = DriverDiscovery.getDriver();
 
     Navigation navigation = new Navigation(driver);
 
